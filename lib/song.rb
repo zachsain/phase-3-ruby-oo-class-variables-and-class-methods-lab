ae7c5a436bff81dfcdee83f381ea325e167dba8b
Zachary Sain <zachsain@gmail.com>
2022-04-05 12:30:56 -0500

    completed

---
+++ lib/song.rb
@@ -0,0 +1,42 @@
+require 'pry'
+
+class Song 
+
+    attr_accessor :name, :artist, :genre
+
+    @@count = 0
+    @@artists = []
+    @@genres = []
+    
+
+    def initialize(name, artist, genre)
+        @name = name
+        @artist = artist
+        @genre = genre
+        @@count += 1 
+        @@genres << genre
+        @@artists << artist
+
+    end 
+
+    def self.count 
+        @@count 
+    end 
+
+    def self.genres
+        @@genres = @@genres.uniq
+    end 
+
+    def self.artists
+        @@artists = @@artists.uniq
+    end 
+
+    def self.genre_count
+        @@genres.tally 
+    end 
+
+    def self.artist_count
+        @@artists.tally
+    end 
+ 
+end 
